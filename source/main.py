7727bd592cf8ee1ce0d630bf9d219e0919d218aa
ducquang2 <quangbilun@gmail.com>
2022-10-11 08:49:30 +0700

    main.py

---
+++ source/main.py
@@ -0,0 +1,139 @@
+import getopt, sys, os
+import matplotlib.pyplot as plt
+
+def visualize_maze(matrix, bonus, start, end, route=None):
+    """
+    Args:
+      1. matrix: The matrix read from the input file,
+      2. bonus: The array of bonus points,
+      3. start, end: The starting and ending points,
+      4. route: The route from the starting point to the ending one, defined by an array of (x, y), e.g. route = [(1, 2), (1, 3), (1, 4)]
+    """
+    #1. Define walls and array of direction based on the route
+    walls=[(i,j) for i in range(len(matrix)) for j in range(len(matrix[0])) if matrix[i][j]=='x']
+
+    if route:
+        direction=[]
+        for i in range(1,len(route)):
+            if route[i][0]-route[i-1][0]>0:
+                direction.append('v') #^
+            elif route[i][0]-route[i-1][0]<0:
+                direction.append('^') #v        
+            elif route[i][1]-route[i-1][1]>0:
+                direction.append('>')
+            else:
+                direction.append('<')
+
+        direction.pop(0)
+
+    #2. Drawing the map
+    ax=plt.figure(dpi=100).add_subplot(111)
+
+    for i in ['top','bottom','right','left']:
+        ax.spines[i].set_visible(False)
+
+    plt.scatter([i[1] for i in walls],[-i[0] for i in walls],
+                marker='X',s=100,color='black')
+    
+    plt.scatter([i[1] for i in bonus],[-i[0] for i in bonus],
+                marker='P',s=100,color='green')
+
+    plt.scatter(start[1],-start[0],marker='*',
+                s=100,color='gold')
+
+    if route:
+        for i in range(len(route)-2):
+            plt.scatter(route[i+1][1],-route[i+1][0],
+                        marker=direction[i],color='silver')
+
+    plt.text(end[1],-end[0],'EXIT',color='red',
+         horizontalalignment='center',
+         verticalalignment='center')
+    plt.xticks([])
+    plt.yticks([])
+    plt.savefig('./demo.jpg')
+
+    plt.show()
+
+    print(f'Starting point (x, y) = {start[0], start[1]}')
+    print(f'Ending point (x, y) = {end[0], end[1]}')
+    
+    for _, point in enumerate(bonus):
+      print(f'Bonus point at position (x, y) = {point[0], point[1]} with point {point[2]}')
+    
+    return plt
+
+def read_file(file_name: str = './input/maze_map2.txt'):
+    f=open(file_name,'r')
+    n_bonus_points = int(next(f)[:-1])
+    bonus_points = []
+    if (n_bonus_points > 0):
+        for i in range(n_bonus_points):
+            x, y, reward = map(int, next(f)[:-1].split(' '))
+            bonus_points.append((x, y, reward))
+
+    text=f.read()
+    matrix=[list(i) for i in text.splitlines()]
+    f.close()
+
+    return bonus_points, matrix
+
+# bonus_points, matrix = read_file('./input/maze_map2.txt')
+
+# print(f'The height of the matrix: {len(matrix)}')
+# print(f'The width of the matrix: {len(matrix[0])}')
+
+def getStartEndPoint(matrix):
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            if matrix[i][j]=='S':
+                start=(i,j)
+
+            elif matrix[i][j]==' ':
+                if (i==0) or (i==len(matrix)-1) or (j==0) or (j==len(matrix[0])-1):
+                    end=(i,j)
+                    
+            else:
+                pass
+    
+    return start, end
+
+# visualize_maze(matrix,bonus_points,start,end)
+
+def main(argv):
+    input_file, output_file, used_algo = '', '', ''
+    try:
+        options, args = getopt(argv, 'hi:o', ['input = ', 'output = ', 'algo = '])
+    except getopt.GetoptError:
+        print('Command line arguments invalid, try again.')
+        sys.exit(2)
+
+    for option, arg in options:
+        if option == '--input':
+            input_file = arg
+        elif option == '--output':
+            output_file = arg
+        elif option == '--algo':
+            algorithm_used = arg
+
+    if (input_file == '' or output_file == ''):
+        print('Arguments missing, try again.')
+        sys.exit(2)
+
+    bonus_points, matrix = read_file(input_file)
+    start, end = getStartEndPoint(matrix)
+
+    route = []
+
+    if algorithm_used == 'dfs':
+        route = DFS(matrix, start, end).Try()
+    elif algorithm_used == 'bfs':
+        route = BFS(matrix, start, end).Try()
+    elif algorithm_used == 'greedy':
+        route = GreedyBFS(matrix, start, end).Try()
+    elif algorithm_used == 'astar':
+        route = AStar(matrix, start, end).Try()
+    elif algorithm_used == 'bonus':
+        route = Bonus(matrix, start, end, bonus_points).Try()
+    else:
+        print('Algorithm not specified. Printing the map without route to exit..')
