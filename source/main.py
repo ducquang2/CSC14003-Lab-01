ae5f0fa3fd9e1facfa3560f8715f3630d3e85ba0
TinDaKing <tranhoangtin3702@gmail.com>
2022-10-20 13:49:16 +0700

    ok

--- source/main.py
+++ source/main.py
@@ -1,3 +1,4 @@
+import sys, getopt
 from implement import *
 from BFS import *
 from DFS import *
@@ -5,38 +6,49 @@ from UCS import *
 from GBFS import *
 from A_SAO import *
 
-PATH = './input/map2.txt'
+# PATH = './input/level_2/input3.txt'
 
-def main():
-    bonus_points, matrix = read_file(PATH)
-    # print(f'The height of the matrix: {len(matrix)}')
-    # print(f'The width of the matrix: {len(matrix[0])}')
+def main(argv):
+    in_file, out_file = './input/', ''
+    try:
+        opts, argv = getopt.getopt(argv, 'hi:o', ['input=', 'output='])
+    except getopt.GetoptError:
+        print('Error command')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '--in':
+            in_file += arg
+        elif opt == '--out':
+            out_file += arg
+
+    bonus_points, matrix = read_file(in_file)
     start, end = getStartEndPoint(matrix)
-    # print(matrix, bonus_points, start, end)
-    out_put = './output/BFS.jpg'
+    
+    # out_put = './output/BFS.jpg'
     route,explored,cost = BFS(matrix,start,end,bonus_points)
     write_cost_path(cost)
-    visualize_maze(matrix,bonus_points,start,end,out_put,route,explored)
+    visualize_maze(matrix,bonus_points,start,end,out_file,route,explored)
 
-    out_put = './output/DFS.jpg'
+    # out_put = './output/DFS.jpg'
     route,explored,cost = DFS(matrix,start,end,bonus_points)
     write_cost_path(cost)
-    visualize_maze(matrix,bonus_points,start,end,out_put,route,explored)
+    visualize_maze(matrix,bonus_points,start,end,out_file,route,explored)
 
-    out_put = './output/UCS.jpg'
+    # out_put = './output/UCS.jpg'
     route,explored,cost = UCS(matrix,start,end,bonus_points)
     write_cost_path(cost)
-    visualize_maze(matrix,bonus_points,start,end,out_put,route,explored)
+    visualize_maze(matrix,bonus_points,start,end,out_file,route,explored)
 
-    out_put = './output/GBFS.jpg'
+    # out_put = './output/GBFS.jpg'
     route,explored,cost = GBFS(matrix,start,end,bonus_points)
     write_cost_path(cost)
-    visualize_maze(matrix,bonus_points,start,end,out_put,route,explored)
+    visualize_maze(matrix,bonus_points,start,end,out_file,route,explored)
 
-    out_put = './output/AStar.jpg'
+    # out_put = './output/AStar.jpg'
     route,explored,cost = AStar(matrix,start,end,bonus_points)
     write_cost_path(cost)
-    visualize_maze(matrix,bonus_points,start,end,out_put,route,explored)
+    visualize_maze(matrix,bonus_points,start,end,out_file,route,explored)
 
 if __name__ == '__main__':
     main()
